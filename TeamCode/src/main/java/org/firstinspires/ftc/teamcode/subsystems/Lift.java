a51d02cd0d794ced74d485bc4b634eb31bd438ad
mattman2864 <mattman2864@gmail.com>
2023-03-17 11:59:54 -0400

    Added more lift functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -6,19 +6,37 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Lift {
 
     // instantiate lift motor
-    private DcMotor lift5rear;
-    private DcMotor lift4front;
+    private DcMotor rearLift;
+    private DcMotor frontLift;
 
     // define lift positions
     final private int[] polePos = {0, 610, 1075, 1460};
     final private  int[] coneStackPos = {0, 45, 90, 135, 180, 360};
 
+    int PolePosition;
+
     public Lift(HardwareMap hardwareMap){
-        lift4front = hardwareMap.get(DcMotor.class, "lift4front");
-        lift5rear = hardwareMap.get(DcMotor.class, "lift5rear");
+        frontLift = hardwareMap.get(DcMotor.class, "lift4front");
+        rearLift = hardwareMap.get(DcMotor.class, "lift5rear");
     }
 
-    public void setPosition(int newPos){
-
+    public void setPosition(int newPos, double power){
+        frontLift.setPower(power);
+        rearLift.setPower(power);
+        rearLift.setTargetPosition(newPos);
+        frontLift.setTargetPosition(-newPos);
     }
+
+    public void setPolePosition(int newPolePos){
+        this.setPosition(polePos[newPolePos], 1);
+        PolePosition = newPolePos;
+    }
+
+    public void checkForZero(){
+        if (PolePosition == 0 && Math.abs(rearLift.getCurrentPosition()) < 10){
+            frontLift.setPower(0);
+            rearLift.setPower(0);
+        }
+    }
+    
 }
