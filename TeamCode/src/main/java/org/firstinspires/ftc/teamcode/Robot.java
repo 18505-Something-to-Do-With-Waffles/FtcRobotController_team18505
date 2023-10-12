5caddb7aa2289edaed5cec0ec274cd7c5d8c8e71
mattman2864 <mattman2864@gmail.com>
2023-10-11 20:14:39 -0400

    skeleton layout

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 // For IMU
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-
+// For Camera
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -36,10 +36,45 @@ public class Robot {
 
     // Instantiate Telemetry
     private Telemetry telemetry;
+    private Intake intake;
 
     public Robot(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
     }
 }
 
-// Define classes for other parts of robot here:
+class Intake {
+    static public double speed;
+    static void Intake(double motorSpeed) {
+        speed = motorSpeed;
+    }
+    private void on() {
+        // TODO: DC Motor Set to speed
+    }
+    private void off() {
+        // TODO: DC Motor set to 0
+    }
+}
+
+class Lift {
+    static public double speed;
+    static void Lift (double motorSpeed) {
+        speed = motorSpeed;
+    }
+    void up() {
+        // TODO: Set lift motors to speed
+    }
+    void down() {
+        // TODO: Set Lift motors to -speed
+    }
+    void setPosition() {
+        // TODO: up() or down() until position is correct
+    }
+}
+
+class Launcher {
+    static public double speed;
+    static void Launcher (double motorSpeed) {
+        speed = motorSpeed;
+    }
+}
