bde4f289782c87ec3ff64831f0a388d53133bff7
mattman2864 <mattman2864@gmail.com>
2023-11-08 19:57:19 -0500

    more restructuring before starting to work on main program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -5,11 +5,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-// For Controller
-import com.qualcomm.robotcore.hardware.Gamepad;
-// For IMU
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 // For Camera
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -25,56 +20,80 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.Tfod;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaCurrentGame;
 
-// For Distance Sensor
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-import java.util.List;
-import java.lang.*;
-
 public class Robot {
 
     // Instantiate Telemetry
     private Telemetry telemetry;
-    private Intake intake;
 
+    // Instantiate Robot Systems
+    private Intake intake;
+    private Lift lift;
+    private Launcher launcher;
     public Robot(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
+        intake = new Intake(hardwareMap);
+        lift = new Lift(hardwareMap, 0.5);
+        launcher = new Launcher(hardwareMap);
     }
 }
 
 class Intake {
-    static public double speed;
-    static void Intake(double motorSpeed) {
-        speed = motorSpeed;
+    private DcMotor intake;
+    public Intake(HardwareMap hardwareMap) {
+        intake = hardwareMap.get(DcMotor.class, "intake");
     }
-    private void on() {
-        // TODO: DC Motor Set to speed
+    public void on(double speed) {
+        intake.setPower(speed);
     }
-    private void off() {
-        // TODO: DC Motor set to 0
+    public void off() {
+        intake.setPower(0);
     }
 }
 
 class Lift {
-    static public double speed;
-    static void Lift (double motorSpeed) {
-        speed = motorSpeed;
+    private DcMotor lift;
+    double power;
+    public Lift (HardwareMap hardwareMap, double globalPower) {
+        // Initializing lift motor and power
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        power = globalPower;
+
+        // Resetting Encoder of lift motor and setting it to "RUN_TO_POSITION" mode
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setTargetPosition(0);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
-    void up() {
-        // TODO: Set lift motors to speed
+    void up(int speed) {
+        lift.setTargetPosition(lift.getTargetPosition()+speed);
     }
-    void down() {
-        // TODO: Set Lift motors to -speed
+    void down(int speed) {
+        lift.setTargetPosition(lift.getTargetPosition()-speed);
     }
-    void setPosition() {
-        // TODO: up() or down() until position is correct
+    void off() {
+        lift.setPower(0);
+    }
+    void setPosition(int encoderPosition) {
+        lift.setTargetPosition(encoderPosition);
+    }
+    void checkForZero() {
+        if (lift.getTargetPosition() == 0 && Math.abs(lift.getCurrentPosition()) < 10) {
+            lift.setPower(0);
+        }
+        else {
+            lift.setPower(power);
+        }
     }
 }
 
 class Launcher {
-    static public double speed;
-    static void Launcher (double motorSpeed) {
-        speed = motorSpeed;
+    DcMotor launch;
+    public Launcher (HardwareMap hardwareMap) {
+        launch = hardwareMap.get(DcMotor.class, "launch");
+    }
+    public void shoot (double speed) {
+        launch.setPower(speed);
+    }
+    public void off (double speed) {
+        launch.setPower(0);
     }
 }
